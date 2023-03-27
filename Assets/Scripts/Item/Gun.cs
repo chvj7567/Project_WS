5fc280514aff309338f99a9e70870aaaaf2303ed
DESKTOP-U0LI16N\User <chvj7567@naver.com>
2023-03-27 11:10:40 +0900

    총 딜레이 추가 및 롱 클릭 시 계속 발사되도록 수정

--- Assets/Scripts/Item/Gun.cs
+++ Assets/Scripts/Item/Gun.cs
@@ -1,32 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    public GameObject bulletPrefab;
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Transform bulletSpawnPoint;
+    [SerializeField] float bulletForce = 10f;
+    [SerializeField] float fireDelay = .1f;
 
-    public Transform bulletSpawnPoint;
+    [SerializeField, ReadOnly] float timeSinceLastFire = 0f;
+    [SerializeField, ReadOnly] bool isFire = false;
 
-    public float bulletForce = 10f;
-
-    async void Update()
+    void Update()
     {
-        // 플레이어가 발사 버튼을 눌렀는지 확인
         if (Input.GetButtonDown("Fire1"))
         {
-            // 스폰 지점에서 새 총알 생성
-            GameObject bullet = CHMMain.Resource.Instantiate(bulletPrefab, bulletSpawnPoint);
-            bullet.transform.localPosition = Vector3.zero;
-            bullet.transform.up = bulletSpawnPoint.transform.up;
-
-            // 총알을 스폰 지점의 방향으로 힘을 가해 발사
-            bullet.GetComponent<Rigidbody>().AddForce(bulletSpawnPoint.up * bulletForce, ForceMode.Impulse);
-
-            await Task.Delay(2000);
-
-            CHMMain.Resource.Destroy(bullet);
+            isFire = true;
         }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            isFire = false;
+        }
+
+        if (timeSinceLastFire < fireDelay)
+        {
+            timeSinceLastFire += Time.deltaTime;
+        }
+        else if(isFire)
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        // 스폰 지점에서 새 총알 생성
+        GameObject bullet = CHMMain.Resource.Instantiate(bulletPrefab, bulletSpawnPoint);
+        bullet.transform.localPosition = Vector3.zero;
+        bullet.transform.up = bulletSpawnPoint.transform.up;
+
+        // 총알을 스폰 지점의 방향으로 힘을 가해 발사
+        bullet.GetOrAddComponent<Bullet>();
+
+        // 총알을 스폰 지점의 방향으로 힘을 가해 발사
+        bullet.GetOrAddComponent<Rigidbody>().AddForce(bulletSpawnPoint.up * bulletForce, ForceMode.Impulse);
+
+        // 딜레이 초기화
+        timeSinceLastFire = 0f;
     }
 }
