b89a2b517a742b17c8889bb3ffce17ba24cf550a
DESKTOP-6O2TLH3\chvj7567 <chvj7567@naver.com>
2023-05-02 22:47:35 +0900

    hp게이지 y축 위치 스케일에 영향 받지 않도록 수정

--- Assets/Scripts/Unit/CHUnitBase.cs
+++ Assets/Scripts/Unit/CHUnitBase.cs
@@ -76,7 +76,8 @@ public class CHUnitBase : MonoBehaviour
                         unitCollider = gameObject.GetOrAddComponent<Collider>();
                     }
 
-                    hpGaugeBar.Init(unitCollider.bounds.size.y / 2);
+                    // 스케일에 비례하게 y축 조정
+                    hpGaugeBar.Init(unitCollider.bounds.size.y / 2f / transform.localScale.x);
                     hpGaugeBar.SetGaugeBar(GetCurrentMaxHp(), GetCurrentHp(), 0f);
                 }
             }
