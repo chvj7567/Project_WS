c52277b70532e1876a82bd7644ee94ec49006891
DESKTOP-6O2TLH3\chvj7567 <chvj7567@naver.com>
2023-05-29 12:52:28 +0900

    유닛 에디터 스크립트 추가

--- Assets/Scripts/Scriptable/UnitData.cs
+++ Assets/Scripts/Scriptable/UnitData.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 public class UnitData : ScriptableObject
 {
     [Header("유닛 정보")]
-    [Tooltip("유닛 이름")] public Defines.EUnit eUnit = Defines.EUnit.None;
-    [Tooltip("유닛 이름 String")] public string unitName = null;
-    [Tooltip("유닛 레벨")] public Defines.ELevel eLevel = Defines.ELevel.None;
-    [Tooltip("최대 HP")] public float maxHp = -1f;
-    [Tooltip("HP 초당 회복량")] public float hpRegenPerSecond = -1f;
-    [Tooltip("최대 MP")] public float maxMp = -1f;
-    [Tooltip("MP 초당 회복량")] public float mpRegenPerSecond = -1f;
-    [Tooltip("공격력")] public float attackPower = -1f;
-    [Tooltip("방어력")] public float defensePower = -1f;
-    [Tooltip("움직일 때 스피드")] public float moveSpeed = -1f;
-    [Tooltip("회전할 때 스피드")] public float rotateSpeed = -1f;
-    [Tooltip("타겟이 없을 경우 타겟 감지 범위")] public float range = -1f;
-    [Tooltip("타겟이 있을 경우 확장되는 감지 배수 범위")] public float rangeMulti = -1f;
-    [Tooltip("타겟 감지 각도")] public float viewAngle = -1f;
+    [Tooltip("유닛 이름"), HideInInspector] public Defines.EUnit eUnit = Defines.EUnit.None;
+    [Tooltip("유닛 이름 String"), HideInInspector] public string unitName = null;
+    [Tooltip("유닛 레벨"), HideInInspector] public Defines.ELevel eLevel = Defines.ELevel.None;
+    [Tooltip("최대 HP"), HideInInspector] public float maxHp = -1f;
+    [Tooltip("HP 초당 회복량"), HideInInspector] public float hpRegenPerSecond = -1f;
+    [Tooltip("최대 MP"), HideInInspector] public float maxMp = -1f;
+    [Tooltip("MP 초당 회복량"), HideInInspector] public float mpRegenPerSecond = -1f;
+    [Tooltip("공격력"), HideInInspector] public float attackPower = -1f;
+    [Tooltip("방어력"), HideInInspector] public float defensePower = -1f;
+    [Tooltip("움직일 때 스피드"), HideInInspector] public float moveSpeed = -1f;
+    [Tooltip("회전할 때 스피드"), HideInInspector] public float rotateSpeed = -1f;
+    [Tooltip("타겟이 없을 경우 타겟 감지 범위"), HideInInspector] public float range = -1f;
+    [Tooltip("타겟이 있을 경우 확장되는 감지 배수 범위"), HideInInspector] public float rangeMulti = -1f;
+    [Tooltip("타겟 감지 각도"), HideInInspector] public float viewAngle = -1f;
 
     [Header("스킬 정보")]
-    [Tooltip("스킬1 이름")] public Defines.ESkill eSkill1 = Defines.ESkill.None;
-    [Tooltip("스킬2 이름")] public Defines.ESkill eSkill2 = Defines.ESkill.None;
-    [Tooltip("스킬3 이름")] public Defines.ESkill eSkill3 = Defines.ESkill.None;
-    [Tooltip("스킬4 이름")] public Defines.ESkill eSkill4 = Defines.ESkill.None;
+    [Tooltip("스킬1 이름"), HideInInspector] public Defines.ESkill eSkill1 = Defines.ESkill.None;
+    [Tooltip("스킬2 이름"), HideInInspector] public Defines.ESkill eSkill2 = Defines.ESkill.None;
+    [Tooltip("스킬3 이름"), HideInInspector] public Defines.ESkill eSkill3 = Defines.ESkill.None;
+    [Tooltip("스킬4 이름"), HideInInspector] public Defines.ESkill eSkill4 = Defines.ESkill.None;
 
     [Header("아이템 정보")]
-    [Tooltip("아이템1 이름")] public Defines.EItem eItem1 = Defines.EItem.None;
+    [Tooltip("아이템1 이름"), HideInInspector] public Defines.EItem eItem1 = Defines.EItem.None;
 
     public UnitData Clone()
     {
